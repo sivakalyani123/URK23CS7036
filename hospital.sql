7a124995484c543fce84fd4e8b1c49ee41ed2272
sivakalyani123 <153582310+sivakalyani123@users.noreply.github.com>
2025-04-29 21:39:33 +0530

    Add files via upload

---
+++ hospital.sql
@@ -0,0 +1,26 @@
+-- hospital.sql
+CREATE DATABASE hospital_db;
+
+USE hospital_db;
+
+CREATE TABLE patients (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    gender VARCHAR(10)
+);
+
+CREATE TABLE doctors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    specialization VARCHAR(100)
+);
+
+CREATE TABLE appointments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    patient_id INT,
+    doctor_id INT,
+    appointment_date DATE,
+    FOREIGN KEY (patient_id) REFERENCES patients(id),
+    FOREIGN KEY (doctor_id) REFERENCES doctors(id)
+);
